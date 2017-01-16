5951e6a3e57dc588f28087d0b4f7926feae975ac
Sengel <m15828654679@163.com>
2017-01-16 16:53:39 +0800

    once gengxin

---
+++ yycgproject/src/main/java/yycg/business/pojo/po/BusinessBasePo.java
@@ -0,0 +1,14 @@
+package yycg.business.pojo.po;
+
+public class BusinessBasePo {
+    private String businessyear;
+
+	public String getBusinessyear() {
+		return businessyear;
+	}
+
+	public void setBusinessyear(String businessyear) {
+		this.businessyear = businessyear;
+	}
+    
+}
